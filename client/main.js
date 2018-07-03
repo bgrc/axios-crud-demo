c5d3656a4486b7f754353f4c3492a9352cfb81d8
xnng <xnng77@gmail.com>
2018-07-03 14:07:40 +0800

    动态从 db.json 中获取数据

--- client/main.js
+++ client/main.js
@@ -1,8 +1,26 @@
 window.onload = function () {
     axios.get('http://localhost:3000/movies')
         .then(function (response) {
-            console.log(response.data)
+            let movies = response.data
+            let str = ``
+
+            for (const movie of movies) {
+                let star = "★★★★★☆☆☆☆☆"
+                let rate = Math.round(movie.score / 2)
+                str += `
+                <li class="movieBox">
+                    <div class="mPost">
+                        <img src="${movie.img}" alt="${movie.title}">
+                    </div>
+                    <div class="mTitle">${movie.title}</div>
+                    <div class="mScore">${star.slice(5 - rate, 10 - rate) + ` ` + movie.score} 分</div>
+                </li>`
+            }
+
+            let moviesRow = document.getElementById("moviesRow")
+            moviesRow.innerHTML += str;
         })
+
         .catch(function (error) {
             console.log(error)
         })
