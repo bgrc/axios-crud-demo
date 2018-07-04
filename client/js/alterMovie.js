7a9e454cebf7a29747b87276db35755f08f560ff
xnng <xnng77@gmail.com>
2018-07-04 21:27:33 +0800

    表单自动填充数据

--- client/js/alterMovie.js
+++ client/js/alterMovie.js
@@ -1,16 +1,11 @@
 window.onload = function () {
-    var sellect = document.getElementById("mySelect");
-    sellect.addEventListener('change', function (e) {
-        let selectId = e.target.value
-        console.log(selectId);
-    }, false)
-
+    let movies
     axios.get('http://localhost:3000/movies')
         .then(function (response) {
-            let movies = response.data
+            movies = response.data
             let html1 = `<option value="">请选择电影</option>`
             let str = ``
-            for (const movie of movies) {
+            for (let movie of movies) {
                 str += `
                 <option value="${movie.id}">${movie.title}</option>`
             }
@@ -19,5 +14,21 @@ window.onload = function () {
         .catch(function (error) {
             console.log(error)
         })
+
+    let sellect = document.getElementById("mySelect");
+    sellect.addEventListener('change', function (e) {
+        let selectId = e.target.value
+
+        let formPlaceholder = document.getElementsByTagName('input')
+        for (let movie of movies) {
+            if (selectId == movie.id) {
+                formPlaceholder[0].placeholder = movie.title
+                formPlaceholder[1].placeholder = movie.img
+                formPlaceholder[2].placeholder = movie.director
+                formPlaceholder[3].placeholder = movie.score
+                formPlaceholder[4].placeholder = movie.date
+            }
+        }
+    }, false)
 
 }
